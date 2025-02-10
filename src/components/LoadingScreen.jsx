4fe5dfd513957d34644e893029fccad7cac8be53
MargoTech <margobartashevich@gmail.com>
2025-02-10 19:10:46 +0100

    fixing the bag

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,8 +1,8 @@
 export const LoadingScreen = () => {
-    return {
-    <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center>
-      <div className="md-4, text-4xl font-mono font-bold">
-      Hello world <span className="animate-blink ml-1"> | </span>
+  return (
+    <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
+      <div className="mb-4 text-4xl font-mono font-bold ">
+        Hello world <span className="animate-pulse ml-1 "> | </span>
       </div>
 
       <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
@@ -11,5 +11,5 @@ export const LoadingScreen = () => {
         </div>
       </div>
     </div>
-  };
+  );
 };
