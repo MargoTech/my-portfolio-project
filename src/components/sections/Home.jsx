cd806ecf7cf65b1303955a944c0634f37e6a7cad
MargoTech <margobartashevich@gmail.com>
2025-02-18 21:13:15 +0100

    Working with Home.jsx

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -3,4 +3,14 @@ export const Home = () => {
       <section
         id="home"
         className="min-h-screen flex items-center justify-center relative"
-      >
+      > 
+      <div className= "text-center z-10 px-4">
+        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">Hi, I'm MargoTech</h1>
+      </div>
+      <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+        I'm beginner frontend developer. I'm study every day and so motivated.
+      </p>
+      <div></div>
+      <section/>
+    );    
+};
