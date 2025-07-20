c2592b49205ff6119e54425edb90fcf20138a2f7
MargoTech <margobartashevich@gmail.com>
2025-07-20 11:09:49 -0100

    Working with portfolio project

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,14 @@ function App() {
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
-      {""}
+
       <div
-        className={`"min-h-screen transition-opacity duration-700 ${
+        className={`min-h-screen transition-opacity duration-700 ${
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-        <Home />
       </div>
     </>
   );
