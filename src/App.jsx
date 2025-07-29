415c4cfe170c73cd688151485ee91bf301e2d7ad
MargoTech <margobartashevich@gmail.com>
2025-07-29 20:12:24 -0100

    WOrking with TailwindCss

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,27 @@
-// import { useState } from "react";
-// import "./App.css";
-// import { LoadingScreen } from "./components/LoadingScreen";
-// import "./index.css";
-// import { Navbar } from "./components/Navbar";
-// import { MobileMenu } from "./components/MobileMenu";
+import { useState } from "react";
+import "./App.css";
+import { LoadingScreen } from "./components/LoadingScreen";
+import "./index.css";
+import { Navbar } from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
 
-// function App() {
-//   const [isLoaded, setIsLoaded] = useState(false);
-//   const [menuOpen, setMenuOpen] = useState(false);
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-//   return (
-
-//     <>
-//       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
-//       <div
-//         className={`min-h-screen transition-opacity duration-700 ${
-//           isLoaded ? "opacity-100" : "opacity-0"
-//         } bg-black text-gray-100`}
-//       >
-//         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-//         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-//       </div>
-//     </>
-//   );
-// }
-
-// export default App;
-
-export default function App() {
   return (
-    <div className="bg-blue-500 text-white text-2xl p-4">
-      Tailwind работает!
-    </div>
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      >
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      </div>
+    </>
   );
 }
+
+export default App;
