ecff4564ae24290a8432a70a238292dc09812419
MargoTech <margobartashevich@gmail.com>
2025-08-05 23:45:31 +0200

    Adding styles to tailwind in Portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        loadingBar: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(200%)" },
+        },
+        fadeOut: {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0, visibility: "hidden" },
+        },
+      },
+      animation: {
+        loadingBar: "loadingBar 1.2s infinite",
+        "fade-out": "fadeOut 0.5s 1s forwards",
+      },
+    },
   },
   plugins: [],
 };
