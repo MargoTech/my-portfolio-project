6633ab76b5c70b6eaa2f4f1fd8dcb0fdcd131450
MargoTech <margobartashevich@gmail.com>
2025-08-10 18:27:06 +0200

    working with Project.jsx

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -13,10 +13,12 @@ export const Projects = () => {
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-              <h3 className="text-xl font-bold mb-2">1 project</h3>
-              <p className="text-gray-400 mb-4">Description of project</p>
+              <h3 className="text-xl font-bold mb-2">
+                Learn Programming With Me App
+              </h3>
+              <p className="text-gray-400 mb-4">Learn coding for beginners</p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Node.js"].map((tech, key) => (
+                {["React", "API"].map((tech, key) => (
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -27,7 +29,6 @@ export const Projects = () => {
                   </span>
                 ))}
               </div>
-
               <div className="flex justify-between items-center">
                 <a
                   href="#"
@@ -37,38 +38,114 @@ export const Projects = () => {
                 </a>
               </div>
             </div>
-            <div
-              className="glass p-6 rounded-xl border border-white/10 
-              hover:-translate-y-1 hover:border-blue-500/30
-              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
-              transition-all"
-            >
-              <h3 className="text-xl font-bold mb-2">Second Project</h3>
-              <p className="text-gray-400 mb-4">Explanation</p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {["ReactJS", "Node.js"].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 
+
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"></div>
+            <h3 className="text-xl font-bold mb-2">Mood Tracker</h3>
+            <p className="text-gray-400 mb-4">Track your moods</p>
+            <div className="flex flex-wrap gap-2 mb-4">
+              {["React", "API"].map((tech, key) => (
+                <span
+                  key={key}
+                  className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                                    "
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+
+            <div className="flex justify-between items-center">
+              <a
+                href="#"
+                className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+              >
+                View Project
+              </a>
+            </div>
+          </div>
+
+          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+            <h3 className="text-xl font-bold mb-2">Chat App</h3>
+            <p className="text-gray-400 mb-4">Chat with the bot</p>
+            <div className="flex flex-wrap gap-2 mb-4">
+              {["React", "API"].map((tech, key) => (
+                <span
+                  key={key}
+                  className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                                    "
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+
+            <div className="flex justify-between items-center">
+              <a
+                href="#"
+                className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+              >
+                View Project
+              </a>
+            </div>
+          </div>
+
+          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+            <h3 className="text-xl font-bold mb-2">
+              Learn Programming With Me App
+            </h3>
+            <p className="text-gray-400 mb-4">Learn coding for beginners</p>
+            <div className="flex flex-wrap gap-2 mb-4">
+              {["React", "API"].map((tech, key) => (
+                <span
+                  key={key}
+                  className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                                    "
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+
+            <div className="flex justify-between items-center">
+              <a
+                href="#"
+                className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+              >
+                View Project
+              </a>
+            </div>
+          </div>
+
+          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+            <h3 className="text-xl font-bold mb-2">Weather App</h3>
+            <p className="text-gray-400 mb-4">
+              Fetches live weather data & adapts UI by conditions
+            </p>
+            <div className="flex flex-wrap gap-2 mb-4">
+              {["ReactJS", "Node.js"].map((tech, key) => (
+                <span
+                  key={key}
+                  className="bg-blue-500/10 text-blue-500 py-1 px-3 
                       rounded-full text-sm
                       transition
                       hover:bg-blue-500/20 hover:-translate-y-0.5
                       hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]"
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-              <div className="flex justify-between items-center">
-                <a
-                  href="#"
-                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
-                  View Project
-                </a>
-              </div>
+                  {tech}
+                </span>
+              ))}
             </div>
-            <div></div>
+            <div className="flex justify-between items-center">
+              <a
+                href="#"
+                className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+              >
+                View Project
+              </a>
+            </div>
           </div>
         </div>
       </RevealOnScroll>
